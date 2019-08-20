b0e9a2c3b59b1023ce1766d5fb1ed03c49acc2c1
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-20 03:48:30 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift
+
+end
+
+def using_pop
+
+end
+
+def pop_with_args
+
+end
+
+def using_shift
+
+end
+
+def shift_with_args
+
+end
+
+def using_concat
+
+end
+
+def using_insert
+
+end
+
+def using_uniq
+
+end
+
+def using_flatten
+
+end
+
+def using_delete
+
+end
+
+def using_delete_at
+
+end
